5154b0d1a7e5598ad03d5b1ff42486257d90bf9a
marilisok <mari.lisok@mail.ru>
2023-03-24 19:31:24 +0300

    profileupdate

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -46,7 +46,7 @@ class ProfilePageBase extends Block<ProfilePageProps> {
     }
 
     if (this.children.profileForm) {
-      // debugger;
+      debugger;
       const inputs = this.children.profileForm.children.inputs.map((element) => {
         const fieldName = element.props.name;
         return new InputContainer({
@@ -60,7 +60,6 @@ class ProfilePageBase extends Block<ProfilePageProps> {
         buttons: this.children.profileForm.children.buttons,
         events: this.children.profileForm.props.events,
       });
-      this.children.profileForm._render();
     }
     return true;
   }
